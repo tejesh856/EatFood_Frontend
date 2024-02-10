83f4dc4a9c225640d68a9e991228c0c48a65e44c
Tejesh Reddy <64296310+tejesh856@users.noreply.github.com>
2024-02-10 17:45:23 +0530

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -5,7 +5,7 @@ export default function Login() {
   let navigate = useNavigate();
   const handlesubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('https://orchid-woodpecker-vest.cyclic.app/api/loginuser',
+    const response = await fetch('https://eat-food-backend.vercel.app/api/loginuser',
       {
         method: 'POST',
         headers: {
