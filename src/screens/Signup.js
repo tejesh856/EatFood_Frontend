50756ba386714b5af2ecb5fa2ba08c483fe07203
Tejesh Reddy <64296310+tejesh856@users.noreply.github.com>
2024-02-10 17:46:14 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -5,7 +5,7 @@ export default function Signup() {
     const navigate = useNavigate();
     const handlesubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch('https://orchid-woodpecker-vest.cyclic.app/api/createuser',
+        const response = await fetch('https://eat-food-backend.vercel.app/api/createuser',
             {
                 method: 'POST',
                 headers: {
